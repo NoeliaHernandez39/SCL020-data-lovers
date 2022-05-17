84438bd316ad13dbcba5d62183aec2fec2b200b6
NoeliaHernandez39 <noah.21riot@yahoo.com>
2022-05-17 08:00:06 -0400

    almost done

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
-import {showNames} from './main.js';
+import {main, showNames} from './main.js'
 //filtra personajes por barra de busqueda
 const textBar = document.getElementById('textBar');
-export const textBarFunc = (myData) => {
+export const textBarFunc = myData => {
     textBar.addEventListener('keyup', () => {
     console.log(textBar.value);
     const searchString = textBar.value.toLowerCase();
 
-    const filteredCharaters = myData.filter((character) => {
+    const filteredCharaters = myData.filter(character => {
         return (
             character.name.toLowerCase().includes(searchString)
         );
@@ -18,19 +18,19 @@ export const textBarFunc = (myData) => {
 
 
 // muestra opciones de filtrado
-export const origins = (myData) => {
+export const origins = myData => {
     const allOrigin = myData.map(element => element.origin.name);
     const everyOrigin = Array.from(new Set(allOrigin));
     return everyOrigin;
 }
 
-export const species = (myData) => {
+export const species = myData => {
     const allSpecies = myData.map(element => element.species);
     const everySpecies = Array.from(new Set(allSpecies));
     return everySpecies;
 }
 
-export const status = (myData) => {
+export const status = myData => {
     const allStatus = myData.map(element => element.status);
     const everyStatus = Array.from(new Set(allStatus));
     return everyStatus;
@@ -39,7 +39,7 @@ export const status = (myData) => {
 // muestra personajes filtrados 
 
 let selectOrigin = document.getElementById('origin-select');
-export const orSelectFunc = (myData) => {
+export const orSelectFunc = myData => {
     selectOrigin.addEventListener('change', () =>{
         let originSelected = selectOrigin.value;
         const filteredOriginCharaters = myData.filter((character) => {
@@ -57,7 +57,7 @@ export const orSelectFunc = (myData) => {
 
 
 let selectSpecies = document.getElementById('species-select');
-export const spSelectFunc = (myData) => {
+export const spSelectFunc = myData => {
     selectSpecies.addEventListener('change', () =>{
         let speciesSelected = selectSpecies.value;
         const filteredSpeciesCharaters = myData.filter((character) => {
@@ -75,7 +75,7 @@ export const spSelectFunc = (myData) => {
 
 
 let selectStatus = document.getElementById('status-select');
-export const stSelectFunc = (myData) => {
+export const stSelectFunc = myData => {
     selectStatus.addEventListener('change', () =>{
         let statusSelected = selectStatus.value;
         const filteredStatusCharaters = myData.filter((character) => {
@@ -91,8 +91,31 @@ export const stSelectFunc = (myData) => {
     });
 }
 
+const selectSort = document.getElementById('sort-select');
+export const soSelecFunc = myData => {
+    selectSort.addEventListener ('click', () => {
+        let selectedSort = selectSort.value;
+        if (selectedSort === 'sortA'){
+            const sortedCharA = myData.sort((a, b) => {
+                if (a.name < b.name) return -1;
+                return 1;
+            });
+            return showChar(sortedCharA);
+        }
+        else if (selectedSort === 'sortD') {
+            const sortedCharD = myData.sort((a, b) => {
+                if (a.name < b.name) return 1;
+                return -1;
+            })
+            return showChar(sortedCharD);
+        }
+        else if (selectedSort === 'sorto'){
+            return showChar(myData);
+        }
+    });
+}
 
-export const showChar = (characters) =>{
+const showChar = characters => {
     const showFilterChar = characters.map((character) => {
         return `
         <div class="firstDiv">
@@ -105,8 +128,7 @@ export const showChar = (characters) =>{
         </div>
         `
     })
-
-  main.innerHTML = showFilterChar.join(''); // tenemos que iterar el resultado del map
+    main.innerHTML = showFilterChar.join('');
 }
 
 
